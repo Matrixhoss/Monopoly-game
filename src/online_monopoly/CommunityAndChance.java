73de34b195fd070f43dc32fa00699df42b424c52
Abdelrahman Ossama <doo2doo2@hotmail.com>
2017-10-17 23:08:41 +0200

    Edited Community and Chance, only the following is remaining:
    1. detect when passing by go (in player class).
    2. get money from all players (multiple players not implemented).
    3. send message to gui class of community and chance (function not yet implemented)

--- src/online_monopoly/CommunityAndChance.java
+++ src/online_monopoly/CommunityAndChance.java
@@ -7,6 +7,7 @@ package online_monopoly;
 
 import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import javax.swing.JOptionPane;
 
@@ -14,6 +15,7 @@ import javax.swing.JOptionPane;
  *
  * @author Abdelrahman Ossama
  */
+
 class Card
 	{
 		public int type;
@@ -21,21 +23,22 @@ class Card
                 public int ammount = 0;
                 public Point p;
                 public boolean used=false;
+                
 
 		public Card(int cardtype, String cardname)
 		{
-			type = cardtype;  // 1 = pay get, 3=goto, 4=return, 5=out of jail
+			type = cardtype;  // 1 = pay,get, 3=goto, 4=return, 5=out of jail, 2 go to jail, 6 get from all, 7 repairs chance, 8 repair comm
 			label = cardname;
 		}
                 public Card(int cardtype, String cardname, int ammount2)
 		{
-			type = cardtype;  // 1 = payget, 3=goto, 4=return, 5=out of jail
+			type = cardtype;  
 			label = cardname;
                         ammount=ammount2;
 		}
                 public Card(int cardtype, String cardname, Point p2)
 		{
-			type = cardtype;  // 1 = pay,get, 3=goto, 4=return, 5=out of jail
+			type = cardtype;  
 			label = cardname;
                         p=p2;
 		}
@@ -48,7 +51,6 @@ public class CommunityAndChance{
     ArrayList<Card> ChestCards;
     
     
-    
     public CommunityAndChance(){
         
         ChanceCards = new ArrayList<Card>();
@@ -59,19 +61,52 @@ public class CommunityAndChance{
         ChanceCards.add(new Card(1,"Pay poor tax of $15",-15));
         ChanceCards.add(new Card(1,"our building {and} loan matures – Collect $150",150));
         ChanceCards.add(new Card(1,"You have won a crossword competition - Collect $100",100));
+        ChanceCards.add(new Card(5,"Get out of Jail Free – This card may be kept until needed, or traded/sold"));
+        ChanceCards.add(new Card(2,"Go to Jail"));
+        ChanceCards.add(new Card(7,"Make general repairs on all your property – For each house pay $25 – For each hotel $100"));
+        ChanceCards.add(new Card(3,"Advance to Illinois Ave. - If you pass Go, collect $200",new Point(6,10)));
+        ChanceCards.add(new Card(3,"Advance to Go (Collect $200)",new Point(0,0)));
+        ChanceCards.add(new Card(3,"Advance to St. Charles Place – If you pass Go, collect $200",new Point(6,10)));
+        ChanceCards.add(new Card(4,"Go Back 3 Spaces"));
+        ChanceCards.add(new Card(3,"Take a trip to Reading Railroad",new Point(5,0)));
+        ChanceCards.add(new Card(3,"Take a walk on the Boardwalk ",new Point(0,1)));
+        ChanceCards.add(new Card(3,"Advance to St. Charles Place – If you pass Go, collect $200",new Point(6,10)));
+        
+
+        
         //ChanceCards.add(new Card(5,"Get out of Jail Free – This card may be kept until needed, or traded/sold\n"));
         
         ChestCards.add(new Card(1,"Bank error in your favor – Collect $200",200));
         ChestCards.add(new Card(1,"Doctor's fees {fee} – Pay $50",-50));
         ChestCards.add(new Card(1,"From sale of stock you get $50",50));
         ChestCards.add(new Card(1,"Holiday {Xmas} Fund matures - Receive {Collect} $100",100));
-        ChestCards.add(new Card(1,"HIncome tax refund – Collect $20",20));
+        ChestCards.add(new Card(1,"Income tax refund – Collect $20",20));
         ChestCards.add(new Card(1,"Life insurance matures – Collect $100",100));
         ChestCards.add(new Card(1,"Pay hospital fees of $100",-100));
         ChestCards.add(new Card(1,"Pay school fees {tax} of $150",150));
         ChestCards.add(new Card(1,"You have won second prize in a beauty contest – Collect $10",10));
         ChestCards.add(new Card(1,"You inherit $100",100));
         ChestCards.add(new Card(1,"Receive $25 consultancy fee",25));
+        ChestCards.add(new Card(3,"Advance to Go (Collect $200)",new Point(0,0)));
+        ChestCards.add(new Card(5,"Get out of Jail Free – This card may be kept until needed, or traded/sold"));
+        ChestCards.add(new Card(2,"Go to Jail"));
+        ChestCards.add(new Card(8,"You are assessed for street repairs – $40 per house – $115 per hotel"));
+        
+        Collections.shuffle(ChanceCards);
+        Collections.shuffle(ChestCards);
+        
+        //void RandomChance(){
+        /*
+    Advance token to the nearest Railroad and pay owner twice the rental to which he/she {he} is otherwise entitled.
+    You have been elected Chairman of the Board – Pay each player $50  */
+        
+        
+   // }
+   // void RandomChest(){
+        /*
+It is your birthday - Collect $10 from each player {Not in the deck}
+  */
+// }
         
         
             
@@ -80,53 +115,75 @@ public class CommunityAndChance{
         this.DrawCard(type,hassan, false);        
     }
     
+    public void ReturnChanceJail(){
+        Card e = new Card(5,"Get Out of Jail Free");
+        ChanceCards.add(e);
+    }
+    public void ReturnChestJail(){
+        Card e = new Card(5,"Get Out of Jail Free");
+        ChestCards.add(e);
+    }
+    
     public void DrawCard(String type, Player hassan, boolean print){
-        
-        
-        Random r = new Random();
-        Card s = new Card(0,"0");
+        Card s;
         if(type.equalsIgnoreCase("chance")){
-            int cardid = r.nextInt(ChanceCards.size());
-            s = ChanceCards.get(cardid);
-            
-                    
+            s = ChanceCards.get(0);
+            ChanceCards.remove(0);
+            if(s.type==5){
+            hassan.addChanceCard(true);
+            return;
+            }
+            ChanceCards.add(s);
         }
         
         else if(type.equalsIgnoreCase("chest")){
-            r.ints(ChestCards.size());
-            int cardid = r.nextInt(ChanceCards.size());
-            s = ChanceCards.get(cardid);
+            s = ChestCards.get(0);
+            ChestCards.remove(0);
+            if(s.type==5){
+            hassan.addChanceCard(true);
+            return;
+            }
+            ChestCards.add(s);
         }
-        else return;
+        else
+            //error
+            return;
         
-        if(s.type==1){
-            hassan.addMoney(s.ammount);
-            JOptionPane.showMessageDialog(null, s.label);
+        // check for the chance/community card type and take corresponding action
+        // 1 = pay,get, 3=goto, 4=return, 5=out of jail, 2 go to jail, 6 get from all, 7 repairs chance, 8 repair comm
+        switch (s.type) {
+            case 1:
+                hassan.addMoney(s.ammount);
+                break;
+            case 2:
+                hassan.goToJail();
+                break;
+            case 3:
+                hassan.setPosition(s.p.getX(), s.p.getY());
+                break;
+            case 4:
+                hassan.moveBack(3);
+                break;
+            case 6:
+                // get money from all players somehow.
+                return;
+            case 7:
+                hassan.payMoney(25*hassan.getTotalHouses());
+                hassan.payMoney(100*hassan.getTotalHotels());
+                return;
+            case 8:
+                hassan.payMoney(40*hassan.getTotalHouses());
+                hassan.payMoney(115*hassan.getTotalHotels());
+                return;
+            default:
+                break;
         }
+        //here should call the class which will draw gui for card.
+        
+        
     }
     
-    //void RandomChance(){
-        /*Advance to Go (Collect $200)
-Advance to Illinois Ave. - If you pass Go, collect $200 
-Advance to St. Charles Place – If you pass Go, collect $200 
-Advance token to the nearest Railroad and pay owner twice the rental to which he/she {he} is otherwise entitled.
-
-Go Back 3 Spaces
-Go to Jail – Go directly to Jail – Do not pass Go, do not collect $200
-Make general repairs on all your property – For each house pay $25 – For each hotel $100 
-
-Take a trip to Reading Railroad {Take a ride on the Reading} – If you pass Go, collect $200 
-Take a walk on the Boardwalk 
-You have been elected Chairman of the Board – Pay each player $50  */
-   // }
-   // void RandomChest(){
-        /*Advance to Go (Collect $200) <Mr. M strides in 7-league boots>
-Get Out of Jail Free {Get out of Jail, Free} – This card may be kept until needed or sold <A winged Mr. M flutters out of a bird cage>
-Go to Jail – Go directly to jail – Do not pass Go – Do not collect $200 <A truncheon-wielding policeman in a light-colored uniform lifts a surprised Mr M by the collar>
-Grand Opera Night {Opening} – Collect $50 from every player for opening night seats <A wall sign near steps reads "Opera Tonite - 8 PM Sharp"; Mr. M leans against it checking his pocket watch in annoyance>
-It is your birthday - Collect $10 from each player {Not in the deck}
-You are assessed for street repairs – $40 per house – $115 per hotel <Mr. M., supported by his near-ubiquitous cane in his left hand, holds a pick and shovel over his right shoulder>
-  */
-// }
+    
+    
     
 }
