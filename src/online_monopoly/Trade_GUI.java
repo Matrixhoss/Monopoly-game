6006d0b3cd48b739f6275d46aae5b90dea4ba12e
Ahmed Mahmoud Mohamed Rashad <arashad96@gmail.com>
2017-11-16 22:34:05 +0200

    main menu gui

--- src/online_monopoly/Trade_GUI.java
+++ src/online_monopoly/Trade_GUI.java
@@ -1,4 +1,5 @@
 package online_monopoly;
+
 import java.awt.Container;
 import java.awt.event.*;
 import java.awt.geom.Point2D;
@@ -17,76 +18,150 @@ import java.util.TimerTask;
 import javafx.scene.control.CheckBox;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Trade_GUI extends JFrame{
+public class Trade_GUI extends JFrame {
+
     public JLabel background;
     public JLabel yours;
     public JButton trade;
     public JButton cancel;
     public JComboBox plylist;
     public JTextField money;
+    public JTextField my_money;
     public JComboBox property;
     public JComboBox property_money;
+    public JComboBox myproperty_money;
     public JComboBox my_property;
-    public JCheckBox box;
-    public JCheckBox box1;
-    public JCheckBox box2;
-    public JCheckBox box3;
-    public JCheckBox box4;
-    public JCheckBox box5;
-    public JCheckBox box6;
-    public JCheckBox box7;
-    public JCheckBox box8;
-    public JCheckBox box9;
-    public JCheckBox box10;
-    public JCheckBox box11;
-    public JCheckBox box12;
-    public JCheckBox box13;
-    public JCheckBox box14;
-    public JCheckBox box15;
-    public JCheckBox box16;
-    public JCheckBox box17;
-    public JCheckBox box18;
-    public JCheckBox box19;
-    public JCheckBox box20;
-    ImageIcon trade_logo= new ImageIcon(getClass().getResource("mischd/background.jpg"));
-    
-    public Trade_GUI(){
+//    public JCheckBox box;
+//    public JCheckBox box1;
+//    public JCheckBox box2;
+//    public JCheckBox box3;
+//    public JCheckBox box4;
+//    public JCheckBox box5;
+//    public JCheckBox box6;
+//    public JCheckBox box7;
+//    public JCheckBox box8;
+//    public JCheckBox box9;
+//    public JCheckBox box10;
+//    public JCheckBox box11;
+//    public JCheckBox box12;
+//    public JCheckBox box13;
+//    public JCheckBox box14;
+//    public JCheckBox box15;
+//    public JCheckBox box16;
+//    public JCheckBox box17;
+//    public JCheckBox box18;
+//    public JCheckBox box19;
+//    public JCheckBox box20;
+//    public JCheckBox box21;
+//    public JCheckBox box22;
+//    public JCheckBox box23;
+//    public JCheckBox box24;
+//    public JCheckBox box25;
+//    public JCheckBox box26;
+//    public JCheckBox box27;
+    public boolean show = false;
+    int x = 0;
+    int y = 95;
+    int v = 0;
+    int w = 230;
+    public ArrayList<String> myArrayList = new ArrayList<String>();
+    public ArrayList<String> myArrayListprop = new ArrayList<String>();
+
+    ImageIcon trade_logo = new ImageIcon(getClass().getResource("mischd/background.jpg"));
+
+    public Trade_GUI() {
         this.setTitle("Trading");
         this.setResizable(false);
         this.setBounds(300, 300, 600, 400);
         this.setVisible(true);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        
+
         Container c = this.getContentPane();
         c.setLayout(null);
         
-        box = new JCheckBox();
-        box.setBounds(100, 100, 20, 20);
-        box.setSelected(false);
-        c.add(box);
-        
+        if (show = false) {
+            
+        }
+        else {
+            for (int i = 0; i < 27; i++) {
+                myArrayList.add(i, "train");
+            }
+            for (String element : myArrayList) {
+                JCheckBox box = new JCheckBox(element);
+                x = x + 20;
+                box.setBounds(x, y, 20, 20);
+                box.setSelected(false);
+                box.setVisible(true);
+                c.add(box);
+            }
+        }
+            for (int i = 0; i < 27; i++) {
+                myArrayListprop.add(i, "train");
+            }
+            for (String element : myArrayListprop) {
+                JCheckBox box = new JCheckBox(element);
+                v = v + 20;
+                box.setBounds(v, w, 20, 20);
+                box.setSelected(false);
+                box.setVisible(true);
+                c.add(box);
+            }
+//        box = new JCheckBox();
+//        box.setBounds(100, 100, 20, 20);
+//        box.setSelected(false);
+//        c.add(box);
+//        
         String[] ply = {"player 1", "player 2", "player 3", "player 4"};
         plylist = new JComboBox(ply);
         plylist.setBounds(400, 50, 120, 40);
         plylist.setSelectedIndex(3);
         plylist.addActionListener(plylist);
         c.add(plylist);
-        
-        my_property = new JComboBox();
-        my_property.setBounds(225, 200, 120, 40);
-        my_property.addActionListener(my_property);
-        c.add(my_property);
+
+//        my_property = new JComboBox();
+//        my_property.setBounds(225, 190, 120, 40);
+//        my_property.addActionListener(my_property);
+//        c.add(my_property);
+
+        String[] mypro_mon = {"Money", "Property"};
+        myproperty_money = new JComboBox(mypro_mon);
+        myproperty_money.setBounds(225, 190, 120, 40);
+        myproperty_money.setSelectedIndex(1);
+        myproperty_money.addActionListener(myproperty_money);
+        myproperty_money.addActionListener(
+                new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox cbx = (JComboBox) e.getSource();
+                String option = (String) cbx.getSelectedItem();
+                if (option.equals("Money")) {
+                    my_money.setVisible(true);
+                    show = false;
+                    
+                } else {
+                    show = true;
+                    my_money.setVisible(false);
+                }
+            }
+        }
+        );
+        c.add(myproperty_money);
         
         property = new JComboBox();
         property.setBounds(250, 50, 120, 40);
         property.setVisible(false);
         property.addActionListener(property);
         c.add(property);
-        
+
         money = new JTextField("Enter amount");
         money.setBounds(250, 50, 120, 40);
         money.setVisible(false);
         c.add(money);
+
+        my_money = new JTextField("Enter amount");
+        my_money.setBounds(355, 190, 120, 40);
+        my_money.setVisible(false);
+        c.add(my_money);
         
         String[] pro_mon = {"Money", "Property"};
         property_money = new JComboBox(pro_mon);
@@ -94,45 +169,45 @@ public class Trade_GUI extends JFrame{
         property_money.setSelectedIndex(1);
         property_money.addActionListener(property_money);
         property_money.addActionListener(
-            new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JComboBox cb = (JComboBox)e.getSource();
-                    String option = (String)cb.getSelectedItem();
-                    if(option.equals("Money")){
-                        money.setVisible(true);
-                        property.setVisible(false);
-                    }
-                    else{
-                        property.setVisible(true);
-                        money.setVisible(false);
-                    }
+                new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox cb = (JComboBox) e.getSource();
+                String option = (String) cb.getSelectedItem();
+                if (option.equals("Money")) {
+                    money.setVisible(true);
+                    show = false;
+                    
+                } else {
+                    show = true;
+                    money.setVisible(false);
                 }
             }
+        }
         );
         c.add(property_money);
-        
+
         trade = new JButton();
         trade.setText("Trade");
-        trade.setBounds(150,300,120,40);
+        trade.setBounds(150, 300, 120, 40);
         c.add(trade);
-        
+
         cancel = new JButton();
         cancel.setText("Cancel");
-        cancel.setBounds(300,300,120,40);
+        cancel.setBounds(300, 300, 120, 40);
         cancel.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    //Trade_GUI.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-                }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Trade_GUI.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
             }
+        }
         );
         c.add(cancel);
-        
-        yours = new JLabel("Your property to exchange");
-        yours.setBounds(50, 200, 180, 40);
+
+        yours = new JLabel("Your property/money to exchange");
+        yours.setBounds(20, 200, 200, 40);
         c.add(yours);
-        
+
         background = new JLabel(trade_logo);
         background.setBounds(0, 0, 600, 400);
         c.add(background);
